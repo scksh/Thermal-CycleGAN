10b6e3cd65f5951a4c35c6633b6c351ec26450d3
SeungHwan Kim <75053202+scksh@users.noreply.github.com>
2025-07-29 10:44:06 +0900

    Update networks.py

--- models/networks.py
+++ models/networks.py
@@ -109,7 +109,7 @@ def init_net(net, init_type='normal', init_gain=0.02, gpu_ids=[]):
 
     Return an initialized network.
     """
-    if len(gpu_ids) > 0:
+    if len(gpu_ids) > 0 and gpu_ids[0] >= 0:
         assert(torch.cuda.is_available())
         net.to(gpu_ids[0])
         net = torch.nn.DataParallel(net, gpu_ids)  # multi-GPUs
